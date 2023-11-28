38a8fac7d3ecf7bdc9887d8716ce21e035c1e567
Dangerprobots <110118097+Dangerprobots@users.noreply.github.com>
2023-11-28 18:08:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,8 @@ from pytgcalls import idle as pyidle
 from config import bot, call_py
 
 bot.start()
-print("STARTING UBOT CLIENT")
-    await bot.start()
-    print("STARTING PYTGCALLS CLIENT")
-    await call_py.start()
-    print(
-        """
-    ------------------------
-   | MusicUserbot Actived! |
-    ------------------------
-"""
-    )
-#print("UserBot Started")
-#call_py.start()
-#print("Vc Client Started")
+print("UserBot Started")
+call_py.start()
+print("Vc Client Started")
 pyidle()
 idle()
